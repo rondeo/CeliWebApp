1ce8ab6ffc504a6ad22716f2805b8fd3e8b8174e
Juan Luis Quirós <jl.quiros.cassillas@gmail.com>
2018-08-20 00:58:02 +0200

    changing design to grid

--- pages/inspire/index.vue
+++ pages/inspire/index.vue
@@ -1,98 +1,85 @@
 <template>
-  <v-layout column  xs12 pa-0>
-    <v-layout row xs12 height="1000">
-      <v-flex row>
-        <v-flex row pa-0  xs12>
-          <v-parallax xs12 src="/cocina.jpg"  
-          height="800"
-          >
-            <v-layout row justify-center mt-5 >
-              <v-flex xs8 mt-5 text-xs-center >
-                <v-text-field v-on:keyup="onChange" v-model="search"
+<v-container xs12>
+  <v-layout center xs12>
+      <v-flex  xs6 px-1>
+        <v-text-field v-on:keyup="onChange" v-model="search"
                 label="Busca tu restaurante sin gluten"
                 persistent-hint
                 color="orange"
                 box
-                ></v-text-field>
-                <v-text-field v-on:keyup="onChange"
-                label="Busca tu restaurante sin gluten"
-                persistent-hint
-                color="orange"
-                box
-                id="2"
-                :value="search"
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-parallax>
-        </v-flex>
+                />
       </v-flex>
-    </v-layout>
-    <v-flex>
-                  <v-list two-line>
-                    <template v-for="item in businesses">
-                      <v-subheader
-                        v-if="item.cityName"
-                        :value="item.cityName"
-                        :key="item.id"
-                      >
-                      </v-subheader>
-                      <v-list-tile
-                        :value="item.name"
-                        :key="item.id"
-                      >
-                        <v-list-tile-content>
-                          <v-list-tile-title v-html="item.name"></v-list-tile-title>
-                          <v-list-tile-sub-title v-html="item.cityName"></v-list-tile-sub-title>
-                        </v-list-tile-content>
-                      </v-list-tile>
-                    </template>
-                  </v-list>
-              </v-flex>
+       <v-flex  xs6 px-1>
+        <v-text-field 
+                label="Busca en una localidad en concreto"
+                persistent-hint
+                color="orange"
+                box
+                />
+      </v-flex>
   </v-layout>
+  <v-layout wrap>
+     <template v-for="(item) in businesses"  v-if="item.id != ''">
+       <v-flex xs12 md4 px-1 mb-1 v-bind:key="item.id"  >
+          <v-card>
+             <v-card-media
+                class="white--text"
+                height="200px"
+                v-bind:src="item.photo" v-bind:key="item.id"
+              >
+                <v-layout justify-end mr-2 mt-5 v-if="item.average < 999" class="black--text">
+                  {{item.average}}
+                </v-layout>
+             </v-card-media>
+              <v-container fill-height fluid ma-0>
+                <v-layout row fill-height wrap>
+                  <v-flex xs12 align-end flexbox>
+                    <span class="headline" v-text="item.name"></span><br>
+                    {{item.cityName}}({{item.stateName}})
+                  </v-flex>
+                </v-layout>
+              </v-container>
+          </v-card>
+       </v-flex>
+     </template>
+ </v-layout>
+</v-container>
 </template>
 
 <script>
-
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-import Logo from '~/components/Logo.vue'
-import axios from 'axios';
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import Logo from "~/components/Logo.vue";
+import axios from "axios";
 
 export default {
-   components: {
+  components: {
     Logo,
     VuetifyLogo
   },
-  props: {
-    searchType: {
-      type: Object,
-      required: false
-    }
-  },
   data() {
     return {
-      search: this.searchType
-        ? {...this.searchType}
-        : "",
       businesses: [
         {
-          id : "",
+          id: "",
           name: "",
           city: ""
         }
       ]
-    }
+    };
   },
   methods: {
     onChange() {
       console.log(this.search);
-      axios.get('http://172.104.146.157:8080/api-0.0.1-SNAPSHOT/business/name/' + this.search + '/')
+      axios
+        .get(
+          "https://api.celicidad.net:8081/v1/business/name/" + this.search + "/"
+        )
         .then(result => {
           this.businesses = result.data;
-          console.log(result)
+          console.log(result);
         })
-        .catch(e => console.log(e))
+        .catch(e => console.log(e));
     }
   }
-}
+};
 </script>
