946881f51a8fbacfd399ea535ac1024db30be4e2
Juan Luis Quirós <jl.quiros.cassillas@gmail.com>
2019-02-22 19:09:11 +0100

    init auth

---
+++ store/auth.js
@@ -0,0 +1,41 @@
+import {setAuthToken, resetAuthToken} from '~/utils/auth'
+import cookies from 'js-cookie'
+
+export const state = () => ({
+    user: null
+  })
+  export const mutations = {
+    set_user (store, data) {
+      store.user = data
+    },
+    reset_user (store) {
+      store.user = null
+    }
+  }
+  export const actions = {
+    async fetch ({commit}) {
+        await this.$axios.get('/auth/user/')
+        .then(response => {
+          commit('set_user', response.data)
+          return response
+        })
+        .catch(error => {
+          commit('reset_user')
+          return error
+        })
+    },
+    async login ({commit}, data) {
+       await this.$axios.post('/auth/token/', data)
+        .then(response => {
+          commit('set_user', response.data.token)
+          setAuthToken(response.data.token)
+          cookies.set('x-access-token', response.data.token, {expires: 7})
+        })
+    },
+    async reset ({commit}) {
+      commit('reset_user')
+      resetAuthToken()
+      cookies.remove('x-access-token')
+      return Promise.resolve()
+    }
+  }
